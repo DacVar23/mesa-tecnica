16539716cc7683812a81047d16459b2667d2f1c3
DacVar <daniel_camilo.varon@uao.edu.co>
2024-03-19 23:10:05 -0500

    Update home

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="container-home">
+    <h1>Mesa técnica de cafés especiales del valle del cauca</h1>
+    <p>asds</p>
+    </div>
+  )
+}
+
+export default Home
