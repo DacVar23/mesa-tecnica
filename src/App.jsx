c8c804decb7e5409cf9b9da8382a89f51d2bb4f8
DacVar <daniel_camilo.varon@uao.edu.co>
2024-03-21 02:30:28 -0500

    Ventajas Responsive update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import Ventajas from './components/Ventajas'
 function App() {
 
   return (      
       <div >
         <h1>Mesa tecnica de cafés especiales del valle</h1>
+        <Ventajas></Ventajas>
       </div>
   )
 }
